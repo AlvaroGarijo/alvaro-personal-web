682619968096f1cc0486d74676f076e4a6689caa
AlvaroGarijo <agarijo@padelmanager.com>
2022-10-26 12:14:51 +0200

    cambios en carpetas

--- src/components/navegation/HomePage.js
+++ src/components/navegation/HomePage.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const HomePage = () => {
   return (
-    <div>soy la home</div>
+    <div>
+      <div className='imageCover'>
+        <h1>Alvaro Garijo</h1>
+        <h5>WEB DEVELOPER | WEB DESIGN | PERSONAL TRAINER</h5>
+        <i></i>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
