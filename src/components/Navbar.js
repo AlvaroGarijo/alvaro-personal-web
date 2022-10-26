fb1996838faabbcdbac90b9114d47bb0dfcf68ec
AlvaroGarijo <agarijo@padelmanager.com>
2022-10-26 11:59:11 +0200

    añadido logo a la navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import './Navbar.css';
+import logoAlvaro from './imageLogo/logoAlvaro.png'
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+
+
   return (
     <>
         <nav className='navbarControl'>
             <div className='unorderListContainer'>
                 <ul className='listWrapper'>
+                    <Link to="/" ><img className= 'logoAlvaroWeb' src={logoAlvaro} alt="logoAlvaro"></img></Link>
                     <Link className='navbarList' to="/aboutMe">Sobre mi</Link>
                     <Link className='navbarList' to="/webProjects">Proyectos Web</Link>
                     <Link className='navbarList' to="/design">Diseños</Link>
