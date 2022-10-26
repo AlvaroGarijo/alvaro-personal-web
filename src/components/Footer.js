82533f2354d370c65e555d9ecbf76df953dbfadb
AlvaroGarijo <agarijo@padelmanager.com>
2022-10-26 11:39:04 +0200

    navbar y footer con estilos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 export const Footer = () => {
   return (
-    <div>Soy el footer</div>
+    <div className='footerWrapper'>
+      <div className='organizationFooter'>
+        <ul className='unorderListFooter'>
+          <Link className='footerList' to="/aboutme">Sobre mi</Link>
+          <Link className='footerList' to="/webProjects">Proyectos Web</Link>
+          <Link className='footerList' to="/design">Diseños</Link>
+          <Link className='footerList' to="/trainingCode">Training | Code</Link>
+          <Link className='footerList' to="/contact">Contacto</Link>
+        </ul>
+      </div>
+      <div className='footerParag'>
+        <p className='textFooter'>Alvaro Garijo web developer | personal trainer 2022 &copy; be happy, only have one life.</p>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import './Navbar.css';
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <>
-        <nav>
-            <div className='navbarControl'>
-                <ul>
-                    <Link to="/aboutMe">Sobre mi</Link>
-                    <Link to="/webProjects">Proyectos Web</Link>
-                    <Link to="/design">Diseños</Link>
-                    <Link to="trainingCode">Training | Code</Link>
-                    <Link to="/contact">Contacto</Link>
+        <nav className='navbarControl'>
+            <div className='unorderListContainer'>
+                <ul className='listWrapper'>
+                    <Link className='navbarList' to="/aboutMe">Sobre mi</Link>
+                    <Link className='navbarList' to="/webProjects">Proyectos Web</Link>
+                    <Link className='navbarList' to="/design">Diseños</Link>
+                    <Link className='navbarList' to="trainingCode">Training | Code</Link>
+                    <Link className='navbarList' to="/contact">Contacto</Link>
                 </ul>
             </div>
         </nav>
