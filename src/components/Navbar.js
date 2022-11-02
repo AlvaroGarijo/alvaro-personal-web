ea0717ab339fb98ff6e517375822471b5c31c83a
AlvaroGarijo <agarijo@padelmanager.com>
2022-11-02 11:36:51 +0100

    navbar responsive completa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,26 @@ export const Navbar = () => {
 
   return (
     <>
-        <nav className='navbarControl'>
-            <div className='unorderListContainer'>
-                <ul className='listWrapper'>
-                    <Link to="/" ><img className= 'logoAlvaroWeb' src={logoAlvaro} alt="logoAlvaro"></img></Link>
-                    <Link className='navbarList' to="/aboutMe">Sobre mi</Link>
-                    <Link className='navbarList' to="/webProjects">Proyectos Web</Link>
-                    <Link className='navbarList' to="/design">Diseños</Link>
-                    <Link className='navbarList' to="trainingCode">Training | Code</Link>
-                    <Link className='navbarList' to="/contact">Contacto</Link>
-                </ul>
-            </div>
-        </nav>
+      <div className='respMenu'>
+        <input className='checkControl' type='checkbox'/>
+        <i className="fas fa-bars"></i>
+        <i className="fas fa-times"></i>
+      
+          <nav className='navbarControl'>
+              <div className='unorderListContainer'>
+                  <ul className='listWrapper'>
+                      <Link to="/" ><img className= 'logoAlvaroWeb' src={logoAlvaro} alt="logoAlvaro"></img></Link>
+                      <Link className='navbarList' to="/aboutMe">Sobre mi</Link>
+                      <Link className='navbarList' to="/webProjects">Proyectos Web</Link>
+                      <Link className='navbarList' to="/design">Diseños</Link>
+                      <Link className='navbarList' to="trainingCode">Training | Code</Link>
+                      <Link className='navbarList' to="/contact">Contacto</Link>
+                  </ul>
+              </div>
+          </nav>
+          <Link to="/" ><img className= 'logoResponsive' src={logoAlvaro} alt="logoAlvaro"></img></Link>
+
+      </div>
     </>
   )
 }
