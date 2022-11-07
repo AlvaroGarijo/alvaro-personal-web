b0de789cb5550ce312b7fbc7319d052aa83d217a
AlvaroGarijo <agarijo@padelmanager.com>
2022-11-07 11:25:33 +0100

    cards de diseños controladas en mobile

--- src/components/navegation/Design.js
+++ src/components/navegation/Design.js
@@ -12,7 +12,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                Spiderman
+                <span className='titleFront'>Spiderman</span>
+                <Link className='linkBackMobile' to='/spidermanDesign'>Spiderman</Link> 
               </div>
               <div className="back">
                 <Link className='linkBack' to='/spidermanDesign'>Spiderman</Link> 
@@ -23,7 +24,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Excelsior
+                <span className='titleFront'>Excelsior</span>
+                <Link className='linkBackMobile' to='/buildItPage'>Excelsior</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/buildItPage'>Excelsior</Link>
@@ -34,7 +36,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Janu
+                <span className='titleFront'>Janu</span>
+                <Link className='linkBackMobile' to='/buildItPage'>Janu</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/buildItPage'>Janu</Link>
@@ -45,7 +48,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Covirán
+                  <span className='titleFront'>Covirán</span>
+                  <Link className='linkBackMobile' to='/coviranDesign'>Covirán</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/coviranDesign'>Covirán</Link>
@@ -56,7 +60,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Fénix-Fit
+                  <span className='titleFront'>Fenix-Fit</span>
+                  <Link className='linkBackMobile' to='/fenixFitDesign'>Fenix-Fit</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/fenixFitDesign'>Fenix-Fit</Link>
@@ -67,7 +72,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Escritor
+                <span className='titleFront'>Escritor</span>
+                <Link className='linkBackMobile' to='/escritorDesign'>Escritor</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/escritorDesign'>Escritor</Link>
@@ -78,7 +84,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    PadelManager
+                <span className='titleFront'>PadelManager</span>
+                <Link className='linkBackMobile' to='/padelManagerDesign'>PadelManager</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/padelManagerDesign'>PadelManager</Link>
@@ -90,7 +97,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Laboratorio Dental
+                <span className='titleFront'>Laboratorio Dental</span>
+                <Link className='linkBackMobile' to='/buildItPage'> Laboratorio Dental</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/buildItPage'> Laboratorio Dental</Link>
@@ -101,7 +109,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Asesoría
+                <span className='titleFront'>Asesoría</span>
+                <Link className='linkBackMobile' to='/asesoriaDesign'>Asesoría</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/asesoriaDesign'>Asesoría</Link>
@@ -112,7 +121,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Sarfati Garden
+                <span className='titleFront'>Sarfati Garden</span>
+                <Link className='linkBackMobile' to='/buildItPage'>Sarfati Garden</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/buildItPage'>Sarfati Garden</Link>
@@ -123,7 +133,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Ejercicios Diseño
+                <span className='titleFront'>Ejercicios Code</span>
+                <Link className='linkBackMobile' to='/ejerciciosCodeDesign'>Ejercicios Diseño</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/ejerciciosCodeDesign'>Ejercicios Diseño</Link>
@@ -134,7 +145,8 @@ export const Design = () => {
           <div className="cardBoxDesign">
             <div className="card">
               <div className="front">
-                    Curriculum Vitae
+                  <span className='titleFront'>Curriculum Vitae</span>
+                    <Link className='linkBackMobile' to='/curriculumDesign'>Curriculum Vitae</Link>
               </div>
               <div className="back">
                 <Link className='linkBack' to='/curriculumDesign'>Curriculum Vitae</Link>
